1478a5d7e91b1a8ed66b34bf91f2ae94e7231ef7
PaulinaMontoya <paulinamontoya90@gmail.com>
2019-07-19 14:17:31 -0500

    Cambios pantalla login y muro

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 let muro = document.getElementById("muro");
 let logIn = document.getElementById("logIn");
+let menu = document.getElementById("navigation")
 logIn.style.display = "block";
 muro.style.display="none";
+menu.style.display="none"
 
 const mostrarMuro = () => {
+  menu.style.display="block"
   logIn.style.display = "none";
   muro.style.display = "block";
 };
 
 document.getElementById("botonIngresar").addEventListener("click", mostrarMuro);
+
+const regresarLogin = () => {
+  menu.style.display="none"
+  logIn.style.display = "block";
+  muro.style.display = "none";
+};
+
+document.getElementById("cerrarSesion").addEventListener("click", regresarLogin);
