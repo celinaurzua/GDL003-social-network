d16173a4929725af5957c10d0c3c25cb7e4a7826
PaulinaMontoya <paulinamontoya90@gmail.com>
2019-07-19 15:20:12 -0500

    Se agrego formulario de registro

--- src/main.js
+++ src/main.js
@@ -1,22 +1,35 @@
 let muro = document.getElementById("muro");
 let logIn = document.getElementById("logIn");
 let menu = document.getElementById("navigation")
+let registro = document.getElementById("registroCorreo")
 logIn.style.display = "block";
+registro.style.display = "none"
 muro.style.display="none";
 menu.style.display="none"
 
 const mostrarMuro = () => {
   menu.style.display="block"
-  logIn.style.display = "none";
   muro.style.display = "block";
+  logIn.style.display = "none";
+  registro.style.display = "none"
 };
 
 document.getElementById("botonIngresar").addEventListener("click", mostrarMuro);
 
 const regresarLogin = () => {
-  menu.style.display="none"
   logIn.style.display = "block";
+  menu.style.display="none"
   muro.style.display = "none";
+  registro.style.display = "none"
 };
 
 document.getElementById("cerrarSesion").addEventListener("click", regresarLogin);
+
+const formularioRegistro = () => {
+  registro.style.display = "block"
+  menu.style.display="none"
+  logIn.style.display = "none";
+  muro.style.display = "none";
+};
+
+document.getElementById("botonRegistrar").addEventListener("click", formularioRegistro);
